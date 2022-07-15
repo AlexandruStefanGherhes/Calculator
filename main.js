34ec53e0d2de702076b2217d71735e6912b8f2d2
Baroush <ghesi4ever@gmail.com>
2022-07-15 15:07:14 +0300

    various improvements

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-"use strict";
+const calculator = document.querySelector("calculator");
+const keys = document.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
 
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator__keys");
-const display = calculator.querySelector(".calculator__display");
+keys.addEventListener("click", (e) => {
+  if (e.currentTarget === e.target) return;
 
-keys.addEventListner("click", (e) => {
-  console.log(e.target)
+  const key = e.target;
+  const keyValue = key.textContent;
+  const displayValue = display.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
+
+  //Is this a number key?
+  if (type === "number") {
+    if (displayValue === "0") {
+      display.textContent = keyValue;
+    } else if (previousKeyType === "operator") {
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
+  }
+
+  //Is this an operator?
+  if (type === "operator") {
+    console.log(key);
+    calculator.dataset.previousKeyType = "operator";
+  }
 });
-
